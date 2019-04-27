83343aa34f4dbbbafea1bd9d7d341d63751b9faf
Stanislav Kudryavtsev <skudryavtsev@outlook.com>
2019-04-27 16:02:57 +0300

    Создан MiddleWare DataLayer

---
+++ DataLayer/EFDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Entityes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    class EFDBContext : DbContext
+    {
+        public DbSet<Directory> directories { get; set; }
+    }
+}
---
+++ DataLayer/Entityes/Material.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entityes
+{
+    public class Material
+    {
+        public int DirectoryId { get; set; }
+        public Directory Directory { get; set; }
+    }
+}
